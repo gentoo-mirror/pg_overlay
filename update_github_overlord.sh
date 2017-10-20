6988f49f7f478d3ad40d647dcabe1eab16dd7eec
perfect7gentleman <Perfect007Gentleman@gmail.com>
2017-10-20 21:29:46 +0700

    UPDATES

--- update_github_overlord.sh
+++ update_github_overlord.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git commit -m "updates";git checkout overlord;git add *;git commit -m "$1";git push origin overlord
+git commit -m "$1";git checkout overlord;git add *;git commit -m "$1";git push origin overlord
